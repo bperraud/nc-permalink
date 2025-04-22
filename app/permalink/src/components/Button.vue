f9280b4acd12802ea3b519d3628100344d8e9fb9
bperraud <benjamin.perraudin@ulb.be>
2025-04-22 15:17:54 +0200

    register button in dom

---
+++ app/permalink/src/components/Button.vue
@@ -0,0 +1,21 @@
+// src/components/ShareLinkButton.vue
+<template>
+  <button @click="shareLink" class="btn btn-primary">Create Share Link</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    shareLink() {
+      alert('Create Perma Link Button Clicked');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.btn {
+  margin-top: 10px;
+}
+</style>
+
--- app/permalink/src/main.js
+++ app/permalink/src/main.js
@@ -1,6 +1,38 @@
 import Vue from 'vue'
-import App from './App.vue'
+import ShareLinkButton from './components/Button.vue'
+
+Vue.prototype.OCA = window.OCA
 Vue.mixin({ methods: { t, n } })
 
-const View = Vue.extend(App)
-new View().$mount('#permalink')
+console.info('MyApp: ShareLinkButton init')
+
+let sectionInstance = null
+let props = null
+const View = Vue.extend(ShareLinkButton)
+
+window.addEventListener('DOMContentLoaded', function () {
+	if (OCA.Sharing && OCA.Sharing.ShareTabSections) {
+		OCA.Sharing.ShareTabSections.registerSection((el, fileInfo) => {
+			if (!el || !fileInfo) return
+
+			if (sectionInstance && document.contains(sectionInstance.$el) && props) {
+				props.fileInfo = fileInfo
+			} else {
+				if (sectionInstance) {
+					sectionInstance.$destroy()
+				}
+
+				sectionInstance = new View({ props: { fileInfo } })
+
+				props = Vue.observable({
+					...sectionInstance._props,
+					fileInfo
+				})
+				sectionInstance._props = props
+
+				sectionInstance.$mount(el[0])
+			}
+		})
+	}
+})
+
