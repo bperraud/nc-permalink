3c6d71ea744423a72e4037ade980125a6c435b84
bperraud <74benji@live.fr>
2025-05-28 14:47:01 +0200

    zip-release.sh

---
+++ zip-release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+APP_NAME="permalink"
+VERSION=$(xmllint --xpath "string(//version)" appinfo/info.xml)
+ARCHIVE_NAME="${APP_NAME}-${VERSION}.tar.gz"
+
+
+# Build
+# rm -rf js/*
+# make build-js-production
+
+# Clean previous builds
+rm -f $ARCHIVE_NAME
+
+git archive --format=tar.gz --prefix="${APP_NAME}/${APP_NAME}/" --output="$ARCHIVE_NAME" HEAD
+
+echo "✅ Beta archive created: $ARCHIVE_NAME"
+
+git tag $VERSION
+git push origin main --tags
+
+gh release create $VERSION "${APP_NAME}-${VERSION}.tar.gz"
+
+echo "✅ Release pushed to git"
