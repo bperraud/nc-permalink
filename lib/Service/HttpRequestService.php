a89030b8ab9582c3c5acbf23794222b9fb78194b
bperraud <74benji@live.fr>
2026-04-16 15:01:52 +0200

    fix vendor autoload.php path

--- lib/Service/HttpRequestService.php
+++ lib/Service/HttpRequestService.php
@@ -19,7 +19,7 @@ use OCP\DB\QueryBuilder\IQueryBuilder;
 use OCP\AppFramework\Services\IAppConfig;
 
 # JWT
-require_once \OC_App::getAppPath('permalink') . '/vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
